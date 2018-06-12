185f62f9994473a92c6e5c328165d9b482c25a47
Mateusz Kucharczyk <mateusz_kucharczyk@yahoo.com>
2018-06-12 12:15:13 +0200

    first upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js file
+var name = prompt('Podaj swoje imię');
+alert('Hello, ' + name + '.');
+console.log('Hello, '+ name + '.');
+
+
+
+var age = prompt ('Podaj swój wiek, ' + name + 'Podaj liczbę np, 22.');
+alert('You are ' + age + ' years old, ' + name + '.');
+console.log ('You are ' + age + ' years old, ' + name + '.');
+// Below I have commented on a function call to disable it
+// console.log('The commented will not be called');
+var country = prompt('Podaj swój kraj, ' + name + '.');
+/*alert(country + ' miejsce w którym jesteś, ' + name + '. Podaj swój wiek ' + age + 'podaj kontynent.');
+*/
+console.log(country + ' miejsce w którym jesteś, ' + name + '. Podaj swój wiek ' + age + 'podaj kontynent.');
